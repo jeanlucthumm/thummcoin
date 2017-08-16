37621c64fd12bd21f2c54aba53c5084f16929a54
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-08-16 16:30:09 -0700

    Setup Google Test skeleton

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jean-Luc Thumm on 8/16/17.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
