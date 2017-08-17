e8781070e238ab7f232f0844f7449be291c25ac8
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-08-17 16:38:21 -0700

    Add Event and Transaction class hierarchy

---
+++ src/Event.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jean-Luc Thumm on 8/17/17.
+//
+
+#include "Event.h"
---
+++ src/Event.h
@@ -0,0 +1,14 @@
+//
+// Created by Jean-Luc Thumm on 8/17/17.
+//
+
+#ifndef THUMMCOIN_EVENT_H
+#define THUMMCOIN_EVENT_H
+
+
+class Event {
+
+};
+
+
+#endif //THUMMCOIN_EVENT_H
