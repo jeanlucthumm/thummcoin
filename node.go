2014078e76da8576cc2181db578e3a2c94e735cf
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-09-06 13:30:22 -0700

    Add Peer functionality and channels to Node

--- node.go
+++ node.go
@@ -4,11 +4,27 @@ import (
 	"net"
 	"sync"
 	"log"
+	"fmt"
+	"encoding/gob"
 )
 
 type Node struct {
-	peers map[net.Addr]bool
+	peers map[*peer]bool
 	mux   sync.Mutex
+
+	broadcast chan *message // note consider making public
+	add       chan *peer
+	del       chan *peer
+}
+
+func NewNode() *Node {
+	return &Node{
+		peers: make(map[*peer]bool),
+
+		broadcast: make(chan *message),
+		add:       make(chan *peer),
+		del:       make(chan *peer),
+	}
 }
 
 func (n *Node) Start(addr net.Addr) error {
@@ -26,15 +42,62 @@ func (n *Node) Start(addr net.Addr) error {
 		if err != nil {
 			continue
 		}
-		peer := &Peer{socket: conn}
-		go peer.handle()
+		fmt.Println("New connection:", conn.RemoteAddr()) // DEBUG
+		peer := &peer{socket: conn}
+		go peer.Handle()
 	}
 }
 
-type Peer struct {
-	socket net.Conn
+func (n *Node) handleChannels() {
+	select {
+	case msg := <-n.broadcast:
+		for p := range n.peers {
+			// encode the message and send
+			enc := p.encoder()
+			err := enc.Encode(msg) // Q a way to store encoded form instead of doing it every time?
+			if err != nil {
+			}
+		}
+	case p := <-n.add:
+		n.peers[p] = true
+	}
 }
 
-func (p *Peer) handle() {
+type peer struct {
+	socket net.Conn
+	node   *Node
+}
 
+func (p *peer) Handle() {
+	dec := p.decoder()
+	for {
+		msg := new(message)
+		err := dec.Decode(msg)
+		if err != nil {
+			fmt.Println("Could not read from peer:", p.socket.RemoteAddr(), err) // DEBUG
+		}
+	}
+}
+
+func (p *peer) Write(msg message) error {
+	enc := p.encoder()
+	err := enc.Encode(msg)
+	if err != nil {
+		fmt.Println("Could not write to peer:", p.socket.RemoteAddr(), err) // DEBUG
+	}
+	return err
+}
+
+func (p *peer) encoder() *gob.Encoder {
+	// TODO store this as a field
+	return gob.NewEncoder(p.socket)
+}
+
+func (p *peer) decoder() *gob.Decoder {
+	// TODO store this as a field
+	return gob.NewDecoder(p.socket)
+}
+
+func (p *peer) end() {
+	p.socket.Close()
 }
