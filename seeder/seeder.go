668ee0e372551c2314b78f2da35b7639eda87aba
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-09-18 19:54:40 -0700

    Add seeder handling structure

--- seeder/seeder.go
+++ seeder/seeder.go
@@ -1,1 +1,38 @@
 package seeder
+
+import (
+	"net"
+	"log"
+)
+
+var ips = []string {
+	":8080",
+	":8081",
+}
+
+func Start(network string, loc string) error {
+	addr, err := net.ResolveTCPAddr(network, loc)
+	if err != nil {
+		log.Fatalln("Could not start seeder:", err)
+		return err
+	}
+
+	ln, err := net.Listen(addr.Network(), addr.String())
+	if err != nil {
+		log.Fatalln("Could not start seeder:", err)
+		return err
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		log.Println("New connection:", conn.RemoteAddr())
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+
+}
