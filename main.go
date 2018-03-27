a4336f2d6d9acd6422910224ebd556800353ab75
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2018-03-27 13:14:56 -0700

    Add seed flag to main

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/jeanlucthumm/thummcoin/node"
 	"net"
+	"flag"
 )
 
 func main() {
+	var seedMode = flag.Bool("seed", false, "enable seeding mode")
+	flag.Parse()
+
 	n := node.NewNode()
 	addr, _ := net.ResolveTCPAddr("tcp", ":8080")
 	n.Start(addr)
-	n.Discover()
+
+	if !*seedMode {
+		n.Discover()
+	}
 }
