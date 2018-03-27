970f1e208c32e4d6ad68246423311b18eb75f6e3
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2018-03-27 15:16:28 -0700

    Implement Send() and Receive() in message

--- prot/message.go
+++ prot/message.go
@@ -1,5 +1,10 @@
 package prot
 
+import (
+	"io"
+	"encoding/gob"
+)
+
 const (
 	PING  = iota
 	PLIST
@@ -8,4 +13,22 @@ const (
 type Message struct {
 	ID   int
 	data []byte
+}
+
+// Send encodes the message and writes it to w
+func Send(w io.Writer, message *Message) error {
+	e := gob.NewEncoder(w)
+	return e.Encode(*message)
+}
+
+// Receive reads from r and extracts a message
+func Receive(r io.Reader) (*Message, error) {
+	var m *Message
+	d := gob.NewDecoder(r)
+	err := d.Decode(m)
+	if err != nil {
+		return nil, err
+	} else {
+		return m, nil
+	}
 }
