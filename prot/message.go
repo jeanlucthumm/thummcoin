0f38732a41f84ac4a78d39362c4d6f2c01b34728
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2018-03-27 22:22:12 -0700

    push temp

--- prot/message.go
+++ prot/message.go
@@ -3,6 +3,7 @@ package prot
 import (
 	"io"
 	"encoding/gob"
+	"github.com/golang/protobuf/proto"
 )
 
 const (
@@ -26,6 +27,53 @@ func Receive(r io.Reader) (*Message, error) {
 	var m *Message
 	d := gob.NewDecoder(r)
 	err := d.Decode(m)
+	if err != nil {
+		return nil, err
+	} else {
+		return m, nil
+	}
+}
+
+func DecodeMessage(m *Message) (interface{}, error) {
+	switch m.ID {
+	case PING:
+
+	}
+}
+
+// MakePeerListMessage constructs a peer list message from the given ips
+func MakePeerListMessage(ips []string) (*Message, error) {
+	m := &Message{}erm
+	pl := &PeerList{}
+
+	// populate peer list
+	for _, ip := range ips {
+		pl.Peers = append(pl.Peers, &PeerList_Peer{
+			Address: ip,
+		})
+	}
+
+	// construct message
+	var err error
+	m.ID = PLIST
+	m.Data, err = proto.Marshal(pl)
+	if err != nil {
+		return nil, err
+	} else {
+		return m, nil
+	}
+}
+
+// MakePingMessage constructs a ping message from the given parameters
+func MakePingMessage(from, to string) (*Message, error) {
+	m := &Message{}
+	p := &Ping{}
+	p.From = from
+	p.To = to
+
+	var err error
+	m.ID = PING
+	m.Data, err = proto.Marshal(p)
 	if err != nil {
 		return nil, err
 	} else {
